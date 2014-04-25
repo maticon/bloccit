4298fcd0ca201fbf0015ff86b681603897e2ad63
Matthew Pickle <matt@mati.co>
2014-04-25 00:03:49 +0000

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,45 @@
 require 'faker'
 
-24.times do
-  p = Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
-  3.times do
-    p.comments.create(body: Faker::Lorem.paragraph)
-  end
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+# Create Posts
+50.times do
+  Post.create(
+    user:   users.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph 
+  )
+end
+posts = Post.all
+
+# Create Comments
+100.times do
+  Comment.create(
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
 end
 
+# Create My User
+user = User.new(
+  name:     Faker::Name.name,
+  email:    'matt+bloccit@mati.co',
+  password: 'pickle01'
+)
+user.skip_confirmation!
+user.save
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
