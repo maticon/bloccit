06ae9c6c97aacf97acdaa5810efb1115ade56ecc
Matthew Pickle <matt@mati.co>
2014-06-23 01:36:24 +0000

    Scoped topics for privacy

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.paginate(page: params[:page])
+    @topics = Topic.visible_to(current_user).paginate(page: params[:page])
     authorize @topics
   end
 
@@ -11,6 +11,7 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+    authorize @topic
     @posts = @topic.posts.paginate(page: params[:page])
   end
 
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
   self.per_page = 7
 end
